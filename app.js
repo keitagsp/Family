66863e4c7774c77f9d7ecaa8a758da04caa51c28
keitagsp <83049576+keitagsp@users.noreply.github.com>
2021-05-26 11:14:36 +0900

    fujikawa0526

--- app.js
+++ app.js
@@ -23,12 +23,13 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // cloud firestoreの場所を定義 4人分
-var db_f = firebase.firestore().collection('father')
-var db_m = firebase.firestore().collection('mother')
-var db_c1 = firebase.firestore().collection('child1')
-var db_c2 = firebase.firestore().collection('child2')
+// var db_f = firebase.firestore().collection('father')
+// var db_m = firebase.firestore().collection('mother')
+// var db_c1 = firebase.firestore().collection('child1')
+// var db_c2 = firebase.firestore().collection('child2')
 firebase.analytics();
 
+var db = firebase.firestore();
 
 // 日時をいい感じの形式にする関数
 function convertFromFirestoreTimestampToDatetime(timestamp) {
@@ -42,10 +43,10 @@ function convertFromFirestoreTimestampToDatetime(timestamp) {
   return `${Y}/${m}/${d} ${H}:${i}:${s}`;
 }
 
-// 送信ボタンを押したら、nameとtextとtimeの値をdbに入れる--------------------------
+// 送信ボタンを押したら、値をdbに入れる--------------------------
 $('#save').on('click', function () {
-  const data = {
-    // nameはfirebaseのフィールド名
+  // Add a new document in collection "cities"
+  db.collection('family').doc('father').set({
     height: $('#height').val(),
     weight: $('#weight').val(),
     sel1: $('#sel1').val(),
@@ -59,38 +60,89 @@ $('#save').on('click', function () {
     sel9: $('#sel9').val(),
     sel10: $('#sel10').val(),
     time: firebase.firestore.FieldValue.serverTimestamp(),// 登録日時
-  };
-  // dbにデータを入れる
-  db_f.add(data);
+  })
+    .then(() => {
+      console.log('Document successfully written!');
+    })
+    .catch((error) => {
+      console.error('Error writing document: ', error);
+    });
+});
+
+// get() を使用して単一のドキュメントの内容を取得する
+
+const docRef = db.collection('family').doc('father');
+const data_f = [];
+docRef.get().then((doc) => {
+  if (doc.exists) {
+    console.log('Document data:', doc.data());
+    data_f.push(doc.data());
+    console.log(data_f[0].sel1);
+  } else {
+    // doc.data() will be undefined in this case
+    console.log('No such document!');
+  }
+}).catch((error) => {
+  console.log('Error getting document:', error);
+});
+// $('#sel1').value(docRef.data.sel1);
+
+// $('#sel1').value(data_f[0].sel1);
+
+  // const data = {
+  //   // nameはfirebaseのフィールド名
+  //   height: $('#height').val(),
+  //   weight: $('#weight').val(),
+  //   sel1: $('#sel1').val(),
+  //   sel2: $('#sel2').val(),
+  //   sel3: $('#sel3').val(),
+  //   sel4: $('#sel4').val(),
+  //   sel5: $('#sel5').val(),
+  //   sel6: $('#sel6').val(),
+  //   sel7: $('#sel7').val(),
+  //   sel8: $('#sel8').val(),
+  //   sel9: $('#sel9').val(),
+  //   sel10: $('#sel10').val(),
+  //   time: firebase.firestore.FieldValue.serverTimestamp(),// 登録日時
+  // };
+  // // dbにデータを入れる
+  // db_f.add(data);
   // 空白にする
   // $('#text').val('');
-  alert('データを記録しました');
-});
 
-//  データ受信処理のコード    最後に保存したデータを取得する-----------------------------------
-// snapshotの中、docに欲しい物が入ってる。
-// onSnapshot オンライン上のデータベースでデータの追加・削除・変更が行われたタイミングで{}内が動いてリアルタイムに反映できる
-// querySnapshot chat10の中身全部が入っているところ
-db_f.orderBy('time', 'asc').page_view(function (querySnapshot) {
-  const dataArray = [];
-  // docの中の下記が欲しいからforEachでdocの回数分処理する
-  querySnapshot.docs.forEach(function (doc) {
-    // data→name, text, timeやらが入ってる
-    // ↑２つを dataオブジェクトにする
-    const data = {
-      // id→ドキュメント名（ランダム英数字のやつ）
-      id: doc.id,
-      // data()の中には配列dataのname,text,timeのが入っている
-      data: doc.data(),
-    };
-    // dataオブジェクトをdataArray配列(javasprictの形の配列)に追加(修正や変更はpushじゃない）
-    dataArray.push(data);
-  });
+// $('#sel1').value(dataArray[dataArray.length - 1].data.sel1);
 
-  console.log(dataArray);
-  console.log(dataArray[dataArray.length - 1].data.sel1);
-  $("#sel1").val((dataArray[dataArray.length - 1].data.sel1));
-});
+
+
+// // 単一のドキュメントを作成または上書きするには、set()
+
+// //  データ受信処理のコード    最後に保存したデータを取得する-----------------------------------
+// // snapshotの中、docに欲しい物が入ってる。
+// // onSnapshot オンライン上のデータベースでデータの追加・削除・変更が行われたタイミングで{}内が動いてリアルタイムに反映できる
+// // querySnapshot chat10の中身全部が入っているところ
+// db_f.orderBy('time', 'asc').onSnapshot(function (querySnapshot) {
+//   const dataArray = [];
+//   // docの中の下記が欲しいからforEachでdocの回数分処理する
+//   querySnapshot.docs.forEach(function (doc) {
+//     // data→name, text, timeやらが入ってる
+//     // ↑２つを dataオブジェクトにする
+//     const data = {
+//       // id→ドキュメント名（ランダム英数字のやつ）
+//       id: doc.id,
+//       // data()の中には配列dataのname,text,timeのが入っている
+//       data: doc.data(),
+//     };
+//     // dataオブジェクトをdataArray配列(javasprictの形の配列)に追加(修正や変更はpushじゃない）
+//     dataArray.push(data);
+//   });
+//   console.log(dataArray);
+//   console.log(dataArray[dataArray.length - 1].data.sel1);
+//   db_f_last.add(dataArray);
+
+// });
+
+
+// $("#sel1").val(dataArray[dataArray.length - 1].data.sel1);
 
 // イベント名	イベントを発生させるタイミング	取得が推奨されるパラメータ	パラメータ説明
 // login	ユーザーがログインしたとき	method	ログイン方法（Email Address、Goog
